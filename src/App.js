475b5f254620bf704f83fa9f8beba322d99c5bd7
Navyapriyalal <navyapriyalal2004@gmail.com>
2024-08-03 19:21:06 +0530

    'commited'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import Header from './components/Header'
+import Header from './components/Header';
 import Tasks from './components/Tasks';
-import { useState } from "react"
+import { useState } from "react";
+import AddTask from './components/AddTask';
 
 const App=() => {
+  const[showAddTask,setShowAddTask] = useState(false)
+
   const [tasks,setTasks] = useState([
     {
         id: 1,
@@ -24,17 +27,23 @@ const App=() => {
     }
 ])
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id , ...task}
+    setTasks ([...tasks,newTask])
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !==id ))
   }
 
   const toggleRemainder = (id) => {
-    console.log('Hello')
     setTasks(tasks.map((task) => task.id === id ? { ...task , remainder: !task.remainder } : task))
   }
   return (
     <div className="container">
-      <Header /> 
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} /> 
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemainder}/> : 'No Tasks to show'}
     </div>
   );
